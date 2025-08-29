6a3b207645d3e4a1057d8b4310d9e9777068ac6c
iwu19370-dot <iwu19370@gmail.com>
2025-08-29 15:00:48 -0700

    0.2 excercise

---
+++ Unit Lessons/U0D2/Demo.java
@@ -0,0 +1,15 @@
+// Class is a set of attributes and behaviors
+public class Demo {
+// main is returning void and void is nothing so it's not returning.
+    public static void main(String[] args) {
+        Demo sample = new Demo();
+
+        sample.havingFun();
+    }
+
+    public String havingFun() {
+        return "I'm having fun!";
+
+    }
+
+}
---
+++ Unit Lessons/U0D2/Zombie.java
@@ -0,0 +1,37 @@
+public class Zombie {
+    //instance variables (attributes)
+    //green
+    String skinColor = "Green";
+    //rot
+    boolean isDead = true;
+    //human count
+    int humansEaten = 0;
+
+    //main
+    public static void main(String[] args) {
+        Zombie Lily = new Zombie();
+
+        System.out.println(Lily.growl());
+        System.out.println("Lily ate " + Lily.humansConsumed() + " humans.");
+
+        Zombie Brian = new Zombie();
+
+        System.out.println(Brian.growl());
+        System.out.println("Brian ate " + Brian.humansConsumed() + " humans.");
+    }
+
+    //methods (behaviors)
+    //walk with arms out
+    //eat brains
+    //growl
+    public String growl() {
+        return "grrrrraa";
+    }
+
+    // Challenge: make a new mathos that returns the number of human eaten
+    public int humansConsumed() {
+        return humansConsumed;
+    }
+    
+
+}
