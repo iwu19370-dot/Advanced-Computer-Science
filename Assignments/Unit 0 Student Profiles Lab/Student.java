a611a0dbd73af123db5ad3b27dde264d8d893194
iwu19370-dot <iwu19370@gmail.com>
2025-09-17 14:57:46 -0700

    New assignment for student profile

--- Assignments/Unit 0 Student Profiles Lab/Student.java
+++ Assignments/Unit 0 Student Profiles Lab/Student.java
@@ -1,13 +1,32 @@
 public class Student {
-    private String name = "";
-    private String id = "";
-    private int grade = "";
-    public static void main(String[] args) {
-        public String name() {
-            return name;
-        }
-        Student (String name, int grade){
-            
-        }
+    private String name;
+    private String id;
+    private int grade;
+
+    
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+        this.id = generateId();
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.grade = 10;
+        this.id = generateId();
+    }
+
+    public String toString() {
+        return "" + name + " is a " + grade + "th grade student. Their id is " + id + ".";
+    }
+
+
+    public String generateId() {
+        return (int) (Math.random() * 10000000 ) + "";
+    }
+    
+    public boolean equals(Student other) {
+        return name.equals(other.name) && id.equals(other.id) && this.grade == other.grade;
     }
 }
--- Assignments/Unit 0 Student Profiles Lab/StudentTester.java
+++ Assignments/Unit 0 Student Profiles Lab/StudentTester.java
@@ -1,4 +1,9 @@
 public class StudentTester {
-    Student student = new Student;
+    public static void main(String[] args) {
+        Student soph = new Student("Isabelle", 10);
+        Student junior = new Student("Sal", 10);
+        System.out.println(junior.equals(soph));
+        System.out.println(soph.toString());
+    }
 
 }
